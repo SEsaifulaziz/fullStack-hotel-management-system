8068076ff713bc3b30762053b169201f8f2bc783
Saiful Aziz <gamestopik712@gmail.com>
2026-03-09 00:41:15 +0500

    Add Booking, Room ResponseDto

--- backend/src/main/java/com/devsaif/backend/model/Room.java
+++ backend/src/main/java/com/devsaif/backend/model/Room.java
@@ -1,23 +1,48 @@
 package com.devsaif.backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.apache.commons.lang3.RandomStringUtils;
+
+
 import java.math.BigDecimal;
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity(name = "room")
+@AllArgsConstructor
+@Data
 public class Room {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String roomType;
-
     private BigDecimal roomPrice;
-
     private boolean isBooked = false;
+    @Lob
+    private Blob photo;
 
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<BookedRoom> bookings;
 
     public Room() {
         this.bookings = new ArrayList<>();
+    }
+
+    public void addBooking(BookedRoom bookedRoom) {
+        if (this.bookings == null) {
+            bookings = new ArrayList<>();
+        }
+        bookings.add(bookedRoom);
+        bookedRoom.setRoom(this);
+        isBooked = true;
+        String bookingCode = RandomStringUtils.randomNumeric(6);
+        bookedRoom.setBookingConfirmationCode(bookingCode);
     }
 
 }
--- backend/src/main/java/com/devsaif/backend/response/RoomResponse.java
+++ backend/src/main/java/com/devsaif/backend/response/RoomResponse.java
@@ -1,4 +1,42 @@
 package com.devsaif.backend.response;
 
+
+import com.devsaif.backend.model.BookedRoom;
+import com.devsaif.backend.model.Room;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Base64;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RoomResponse {
+
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked;
+    private String photo;
+    private List<BookingResponse> bookings;
+
+
+    public RoomResponse(Long id, String roomType, BigDecimal roomPrice) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+    }
+
+    public RoomResponse(Long id, String roomType, BigDecimal roomPrice, boolean isBooked,
+                        byte[] photoBytes, List<BookingResponse> bookings) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.isBooked = isBooked;
+        this.photo = photoBytes != null ? Base64.getEncoder().encodeToString(photoBytes) : null;
+        this.bookings = bookings;
+    }
 }
