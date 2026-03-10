f282d85a61c9a83b105a955e731c67875478ed98
Saiful Aziz <gamestopik712@gmail.com>
2026-03-11 00:57:15 +0500

    Modified Room repository

--- backend/src/main/java/com/devsaif/backend/respository/RoomRepository.java
+++ backend/src/main/java/com/devsaif/backend/respository/RoomRepository.java
@@ -2,9 +2,14 @@ package com.devsaif.backend.respository;
 
 import com.devsaif.backend.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room,Long> {
 
+    @Query("SELECT DISTINCT r.roomType FROM room r ")
+    List<String> findDistinctRoomTypes();
 }
