7a03cd35a65663f19a39fdfd3a659486ca96d349
Saiful Aziz <gamestopik712@gmail.com>
2026-03-11 00:58:43 +0500

    Modified Room service

--- backend/src/main/java/com/devsaif/backend/service/RoomService.java
+++ backend/src/main/java/com/devsaif/backend/service/RoomService.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.Blob;
 import java.sql.SQLException;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -32,4 +33,10 @@ public class RoomService implements IRoomService {
 
         return roomRepository.save(room);
     }
+
+    public List<String> getAllRoomTypes() {
+        return roomRepository.findDistinctRoomTypes();
+    }
+
+
 }
