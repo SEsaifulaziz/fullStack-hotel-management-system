b7d069148265df992b2be938b8f8126a6a64e60f
Saiful Aziz <gamestopik712@gmail.com>
2026-03-11 01:00:31 +0500

    Modified ApiFunctions.jsx

--- frontend/src/components/utils/ApiFunctions.js
+++ frontend/src/components/utils/ApiFunctions.js
@@ -6,13 +6,17 @@ export const api = axios.create({
 
 // this function add a new room to the database
 export async function addRoom(photo, roomType, roomPrice) {
-    const formData = new FormData();
-    formData.append("photo", photo);
-    formData.append("roomType", roomType);
-    formData.append("roomPrice", roomPrice);
+    const formData = new FormData()
+    formData.append("photo", photo)
+    formData.append("roomType", roomType)
+    formData.append("roomPrice", roomPrice)
 
-    const response = await api.post("/rooms/add/new-room", formData);
-    if (response.status === 201) {
+    const response = await api.post("/rooms/add/new-room", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        }
+    });
+    if (response.status === 200) {
         return true;
     } else {
         return false;
@@ -23,10 +27,9 @@ export async function addRoom(photo, roomType, roomPrice) {
 // this function gets all room types from the database
 export async function getRoomTypes() {
     try{
-        const response = await api.get("/rooms/room-types");
+        const response = await api.get("/rooms/room/types");
         return response.data;
     } catch(error){
-        throw new Error(error.data = "Error fetching room types")
-        //throw new Error("Error fetching room types")
+        throw new Error("Error fetching room types")
     }
 }
