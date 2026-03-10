5f57c49fe4bb4cc3dfe3a8b44118ed1ef3085b30
Saiful Aziz <gamestopik712@gmail.com>
2026-03-11 01:42:34 +0500

    Add BookingService.java

---
+++ backend/src/main/java/com/devsaif/backend/service/BookingService.java
@@ -0,0 +1,14 @@
+package com.devsaif.backend.service;
+
+import com.devsaif.backend.model.BookedRoom;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookingService {
+
+    public List<BookedRoom> getAllBookingsByRoomId(Long roomId) {
+        return null;
+    }
+}
