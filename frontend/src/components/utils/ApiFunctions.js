fa7ead9c9b0fb81cb784d8da2be40333a87f649c
Saiful Aziz <gamestopik712@gmail.com>
2026-03-11 01:51:39 +0500

    Add getAllRooms function

--- frontend/src/components/utils/ApiFunctions.js
+++ frontend/src/components/utils/ApiFunctions.js
@@ -33,3 +33,15 @@ export async function getRoomTypes() {
         throw new Error("Error fetching room types")
     }
 }
+
+// this function gets all rooms from the database
+export async function getAllRooms(){
+    try{
+        const response = await api.get("/rooms/all-rooms");
+        return response.data;
+    }
+    catch(error){
+        throw new Error("Error fetching rooms")
+    }
+
+}
